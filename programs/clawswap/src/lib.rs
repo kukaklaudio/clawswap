56c706605e41f81c5029d604c26d1f9140c7e47d
Pedro Piccino <pedropiccino@Pedros-Mac-mini.local>
2026-02-08 11:47:20 -0300

    🚀 Deploy success on localnet - Program ID: Eg5dQXRanxjRjfF28KxvSMfnNNgPGMc63HoVYbmTWqAZ
    - Updated program ID in lib.rs and Anchor.toml
    - API scaffold with Express + TypeScript
    - Devnet airdrop rate-limited, using localnet for testing

--- programs/clawswap/src/lib.rs
+++ programs/clawswap/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("FKTdYU5qqErJWkB1k2atg9v8JzwsYNveD2W1jAgoYNAW");
+declare_id!("Eg5dQXRanxjRjfF28KxvSMfnNNgPGMc63HoVYbmTWqAZ");
 
 #[program]
 pub mod clawswap {
